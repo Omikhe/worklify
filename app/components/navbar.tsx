e0125d895cfe4876627fc2371da77c611ab222ad
Omikhe <msakamike84@gmail.com>
2024-11-28 18:46:40 +0200

    add responsiveness

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,21 +1,110 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
+import { Home, Briefcase, LogIn, UserRound } from 'lucide-react';
+import clsx from "clsx";
 
 export default function Navbar() {
+    const pathname = usePathname();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const navItems = [
+        { title: "Home", href: "/", icon: <Home /> },
+        { title: "Services", href: "#", icon: <Briefcase /> },
+        { title: "Log In", href: "/login", icon: <LogIn /> },
+        { title: "Sign Up", href: "/signup", icon: <UserRound /> }
+    ];
+
     return (
-        <nav className="md:flex md:w-full md:h-20 md:px-28 md:justify-between md:items-center md:font-bold">
-            <div className="md:text-3xl">Worklify<span className="text-[#14E2A1]">.</span></div>
-            <div className="hidden md:text-[24px] md:flex md:justify-between md:items-center md:gap-12 font-medium">
-                <Link href={"#"}>Home</Link>
-                <Link href={"#"}>Services</Link>
-                <Link href={"#"}>Log In</Link>
-                <Link href={"#"} className="md:flex md:justify-center md:w-32 md:h-14 md:px-[16px] md:py-[8px] md:text-white md:items-center md:bg-black md:rounded-[5px]">
-                    <div>
-                        Sign Up
+        <main className="flex justify-between items-center py-6 px-5 max-w-7xl mx-auto w-full">
+            <nav className="md:flex md:w-full md:justify-between md:items-center md:font-bold">
+                <div className="text-2xl font-bold md:text-3xl">Worklify<span className="text-[#14E2A1]">.</span></div>
+                <div className="hidden md:text-[24px] md:flex md:justify-between md:items-center md:gap-12 font-medium">
+                    {navItems.map((item) => (
+                        <Link
+                            key={item.title}
+                            href={item.href}
+                            className={clsx(
+                                "font-semibold text-base text-black active:text-[#14E2A1] transition-colors",
+                                {
+                                    "text-[#14E2A1]": pathname === item.href,
+                                    "hidden md:flex md:justify-center md:w-32 md:h-14 md:px-[16px] md:py-[8px] md:text-white md:items-center md:bg-black md:rounded-[5px] md:text-lg": item.title === "Sign Up"
+                                }
+                            )}
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            {item.title}
+                        </Link>
+                    ))}
+                </div>
+            </nav>
+
+            {/* Mobile Menu Button */}
+            <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="lg:hidden"
+                aria-label="Toggle menu"
+            >
+                <div className="relative w-10 h-10">
+                    <Bars3Icon
+                        className={`absolute transition-all duration-300 ease-in-out ${isMenuOpen ? 'opacity-0 rotate-180' : 'opacity-100 rotate-0'
+                            } h-10 w-10 text-gray-900`}
+                        strokeWidth={2}
+                    />
+                </div>
+            </button>
+
+
+            {/* Mobile Navigation Overlay */}
+            <div
+                className={`fixed inset-0 bg-black/20 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+                    } lg:hidden`}
+                onClick={() => setIsMenuOpen(false)}
+            />
+
+            {/* Mobile Navigation Sliding Menu */}
+            <div
+                className={`fixed rounded-tl-[10px] rounded-bl-[10px] top-0 right-0 h-screen w-64 bg-[#F6F5FA]/50 transform transition-transform duration-300 ease-in-out 
+                ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}
+                shadow-lg z-50 lg:hidden backdrop-blur-lg`}
+            >
+                <div className="flex justify-end p-4">
+                    <button
+                        onClick={() => setIsMenuOpen(false)}
+                        aria-label="Close menu"
+                    >
+                        <XMarkIcon
+                            className="h-10 w-10 text-gray-900"
+                            strokeWidth={2}
+                        />
+                    </button>
+                </div>
+
+                <nav className="px-8 pt-8">
+                    <div className="flex flex-col space-y-8 justify-start">
+                        {navItems.map((item) => (
+                            <Link
+                                key={item.title}
+                                href={item.href}
+                                className={clsx("font-semibold text-base text-black active:text-[#14E2A1] transition-colors", {
+                                    "text-[#14E2A1]": pathname === item.href,
+                                },)}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                <div className="flex gap-2">
+                                    {item.icon}
+                                    {item.title}
+                                </div>
+
+                            </Link>
+                        ))}
                     </div>
-                </Link>
-
-
+                </nav>
             </div>
-        </nav>
+
+
+        </main>
     );
 }
