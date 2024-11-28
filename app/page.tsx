d89396be65c7c46a7012537a804e59bc8045e8f0
Omikhe <msakamike84@gmail.com>
2024-11-28 18:50:13 +0200

    refactor and comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,38 @@
+"use client"
 import Navbar from "./components/navbar";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Navbar></Navbar>
-      <div className="md:flex md:flex-col md:w-full md:h-20 md:px-28 md:justify-center md:items-center md:font-bold md:mt-80">
-        <h1 className="md:text-8xl md:w-[1011px] md:text-center md:flex md:justify-center md:items-center md:mb-8">Unlock Opportunities.
-          Build Your Future.</h1>
+      {/* Navigation bar component */}
+      <Navbar />
+
+      {/* Hero section with centered content */}
+      <div className="flex flex-col space-y-8 justify-center items-center font-bold md:mt-[250px] mt-52">
+        {/* Main headline */}
+        <h1 className="text-5xl md:text-8xl md:w-[1011px] text-center md:flex md:justify-center md:items-center md:mb-8">
+          Unlock Opportunities. Build Your Future.
+        </h1>
+
+        {/* Call-to-action buttons */}
         <div className="flex gap-8">
-
-          <Link href={"#"} className="flex justify-center items-center md:w-40 md:h-14 md:bg-[#0BB574] md:text-white md:px-[32px] md:py-[16px] text-nowrap rounded-[5px]">
-            <div>
-              Offer Services
-            </div>
-          </Link>
-          <Link href={"#"} className="flex justify-center items-center  md:w-40 md:h-14 hover:md:bg-black hover:md:text-white md:px-[32px] md:py-[16px] text-nowrap rounded-[5px] md:border-2 md:border-black transition-colors ease-in-out duration-200">
-            <div>
-              Find Services
-            </div>
+          {/* Offer Services button */}
+          <Link
+            href="#"
+            className="flex justify-center items-center w-[146.48px] h-[44.39px] md:w-40 md:h-14 bg-[#0BB574] text-white md:px-[32px] md:py-[16px] text-nowrap rounded-[5px]"
+          >
+            Offer Services
           </Link>
 
+          {/* Find Services button */}
+          <Link
+            href="#"
+            className="flex justify-center items-center w-[176.66px] h-[44.39px] active:md:bg-black md:w-40 md:h-14 hover:md:bg-black hover:md:text-white md:px-[32px] md:py-[16px] text-nowrap rounded-[5px] border-2 border-black transition-colors ease-in-out duration-200"
+          >
+            Find Services
+          </Link>
         </div>
-
       </div>
     </main>
   );
