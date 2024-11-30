bd17ba74f115378dc623525fe03a9bd630782dcf
Omikhe <msakamike84@gmail.com>
2024-11-30 03:54:19 +0200

    add colors

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
       <Navbar />
 
       {/* Hero section with centered content */}
-      <div className="flex flex-col space-y-8 justify-center items-center font-bold md:mt-[250px] mt-52">
+      <div className="flex flex-col space-y-8 justify-center items-center font-bold md:mt-[250px] mt-52 text-[#05120E]">
         {/* Main headline */}
-        <h1 className="text-5xl md:text-8xl md:w-[1011px] text-center md:flex md:justify-center md:items-center md:mb-8">
+        <h1 className="text-5xl w-80 md:text-8xl md:w-[1011px] text-center md:flex md:justify-center md:items-center md:mb-8">
           Unlock Opportunities. Build Your Future.
         </h1>
 
@@ -28,7 +28,7 @@ export default function Home() {
           {/* Find Services button */}
           <Link
             href="#"
-            className="flex justify-center items-center w-[176.66px] h-[44.39px] active:md:bg-black md:w-40 md:h-14 hover:md:bg-black hover:md:text-white md:px-[32px] md:py-[16px] text-nowrap rounded-[5px] border-2 border-black transition-colors ease-in-out duration-200"
+            className="flex justify-center items-center w-[176.66px] h-[44.39px] active:md:bg-black md:w-40 md:h-14 hover:md:bg-black hover:md:text-white md:px-[32px] md:py-[16px] text-nowrap rounded-[5px] border-2 border-[#text-[#05120E]] transition-colors ease-in-out duration-200"
           >
             Find Services
           </Link>
